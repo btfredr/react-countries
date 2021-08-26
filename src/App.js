b0cfa8bb0d9cf6d403665e6b6a95aed3861b5b4e
Fredrik <fredrikf2000@gmail.com>
2021-08-26 19:20:42 +0200

    added list of countries to table

--- src/App.js
+++ src/App.js
@@ -1,5 +1,55 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { API_URL } from "./utils/constants";
+
 function App() {
-  return <div className="App"></div>;
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getCountries = async () => {
+      const response = await axios(API_URL);
+
+      setCountries(response.data);
+      console.log(response.data);
+      setIsLoading(false);
+    };
+    getCountries();
+  }, []);
+
+  return (
+    <div className="appContainer">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Region</th>
+            <th>Area</th>
+            <th>Population</th>
+          </tr>
+        </thead>
+        <tbody>
+          {countries.map((country) => {
+            return (
+              <tr>
+                <td>
+                  <img
+                    src={country.flag}
+                    alt={country.name}
+                    className="countryFlag"
+                  />
+                  {country.name}
+                </td>
+                <td>{country.region}</td>
+                <td>{country.area}</td>
+                <td>{country.population}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
